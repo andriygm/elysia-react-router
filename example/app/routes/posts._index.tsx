2a4858715007718dfc1cb58ea611bf445752f8b1
Andriy Massimilla <andriygm@icloud.com>
2024-11-15 17:53:58 -0500

    Use future flags in example

--- example/app/routes/posts._index.tsx
+++ example/app/routes/posts._index.tsx
@@ -1,36 +1,36 @@
-import { type LoaderFunctionArgs, json } from "@remix-run/node";
+import { type LoaderFunctionArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 
 export const loader = async ({ context }: LoaderFunctionArgs) => {
-	return json({
-		...context,
-		posts: [
-			{
-				slug: "my-first-post",
-				title: "My First Post",
-			},
-			{
-				slug: "90s-mixtape",
-				title: "A Mixtape I Made Just For You",
-			},
-		],
-	});
+  return {
+    ...context,
+    posts: [
+      {
+        slug: "my-first-post",
+        title: "My First Post",
+      },
+      {
+        slug: "90s-mixtape",
+        title: "A Mixtape I Made Just For You",
+      },
+    ],
+  };
 };
 
 export default function Posts() {
-	const { posts, hotPostName } = useLoaderData<typeof loader>();
+  const { posts, hotPostName } = useLoaderData<typeof loader>();
 
-	return (
-		<main>
-			<h1>Posts</h1>
-			<p>🔥🔥 {hotPostName} 🔥🔥</p>
-			<ul>
-				{posts.map((post) => (
-					<li key={post.slug}>
-						<p>{post.title}</p>
-					</li>
-				))}
-			</ul>
-		</main>
-	);
+  return (
+    <main>
+      <h1>Posts</h1>
+      <p>🔥🔥 {hotPostName} 🔥🔥</p>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <p>{post.title}</p>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
